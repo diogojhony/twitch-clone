a3a63f80b77f61259c5386979e637b0491cc4a24
Diogo Jhony <diogojhony@gmail.com>
2020-08-27 15:24:40 -0300

    chore: Create Following and ComingSoon pages

---
+++ src/pages/ComingSoon/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const ComingSoon: React.FC = () => {
+  return (
+    <Container>
+      <Text>Coming soon...</Text>
+    </Container>
+  );
+};
+
+export default ComingSoon;
